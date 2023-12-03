e2fab4c83a987930be99ed04c2f524a14f22a85e
Lanlinju <lanlinju@outlook.com>
2023-12-03 20:49:20 +0800

    update:更新项目的部分依赖

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -54,9 +54,9 @@ android {
 dependencies {
     implementation(project(":video-player"))
 
-    implementation("androidx.core:core-ktx:1.9.0")
-    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.1")
-    implementation("androidx.activity:activity-compose:1.7.0")
+    implementation("androidx.core:core-ktx:1.10.1")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
+    implementation("androidx.activity:activity-compose:1.8.1")
     implementation(platform("androidx.compose:compose-bom:2023.03.00"))
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-graphics")
@@ -71,7 +71,7 @@ dependencies {
     debugImplementation("androidx.compose.ui:ui-test-manifest")
 
     // navigation component
-    implementation("androidx.navigation:navigation-compose:2.5.0")
+    implementation("androidx.navigation:navigation-compose:2.5.3")
 
     // retrofit + Gson
     implementation("com.squareup.retrofit2:retrofit:2.9.0")
