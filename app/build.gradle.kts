8f3b4cb95fca2b3069a8913c8d05f81b94ba6d4f
Lanlinju <lanlinju@outlook.com>
2023-12-03 23:10:52 +0800

    update:修改最小minSdk为26
    修改原因是使用LocalDate提示要求Api26,偷个懒:)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,7 +11,7 @@ android {
 
     defaultConfig {
         applicationId = "com.sakura.anime"
-        minSdk = 24
+        minSdk = 26
         targetSdk = 34
         versionCode = 1
         versionName = "1.0"
