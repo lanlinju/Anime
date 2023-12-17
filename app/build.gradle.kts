73f92f0a55d0acee7216d46af474f20345c66dfc
Lanlinju <lanlinju@outlook.com>
2023-12-17 23:24:36 +0800

    update:Change version to 1.0.4

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.sakura.anime"
         minSdk = 26
         targetSdk = 34
-        versionCode = 4
-        versionName = "1.0.3"
+        versionCode = 5
+        versionName = "1.0.4"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
