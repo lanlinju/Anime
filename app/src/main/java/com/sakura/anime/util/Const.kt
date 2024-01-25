74a6967223f183d006fd95ae6404a30f628448c8
Lanlinju <lanlinju@outlook.com>
2024-01-25 21:58:28 +0800

    update:更换樱花动漫网站的域名
    原来的网站域名需要翻墙才可以观看

--- app/src/main/java/com/sakura/anime/util/Const.kt
+++ app/src/main/java/com/sakura/anime/util/Const.kt
@@ -1,14 +1,13 @@
 package com.sakura.anime.util
 
-const val BASE_URL = "http://www.yinghuacd.com/"
+// const val BASE_URL = "http://www.yinghuacd.com/"
+const val BASE_URL = "http://www.iyinghua.io"
 const val READ_TIMEOUT = 1L
 
 const val CROSSFADE_DURATION = 500
 const val VIDEO_ASPECT_RATIO = 1.778f
 
 const val LOW_CONTENT_ALPHA= 0.35f
-
-
 
 val TABS = listOf("周一", "周二", "周三", "周四", "周五", "周六", "周日")
 
