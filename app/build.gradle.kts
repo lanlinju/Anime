09b9e2cb37b376e092d6746bb89513e3c97b7b48
Lanlinju <lanlinju@outlook.com>
2024-02-23 15:48:19 +0800

    add: 添加paging分页库依赖

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -104,6 +104,10 @@ dependencies {
     ksp("androidx.room:room-compiler:$room_version")
     implementation("androidx.room:room-ktx:$room_version")
 
+    // paging compose
+    implementation("androidx.paging:paging-runtime:3.2.1")
+    implementation("androidx.paging:paging-compose:3.3.0-alpha03")
+
     // splash screen
     implementation("androidx.core:core-splashscreen:1.0.0")
 }
