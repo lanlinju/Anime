9978519347c51502c92876eb7a0fbac294fb1923
Lanlinju <lanlinju@outlook.com>
2024-03-31 11:09:06 +0800

    chore: 修改Github仓库的域名

--- app/src/main/java/com/sakura/anime/util/Const.kt
+++ app/src/main/java/com/sakura/anime/util/Const.kt
@@ -9,8 +9,8 @@ const val KEY_FROM_LOCAL_VIDEO = "fromLocalVideo"
 
 val TABS = listOf("周一", "周二", "周三", "周四", "周五", "周六", "周日")
 
-const val GITHUB_ADDRESS = "https://github.com/Lanlinju/Anime"
-const val CHECK_UPDATE_ADDRESS = "https://api.github.com/repos/Lanlinju/Anime/releases/latest"
+const val GITHUB_ADDRESS = "https://github.com/lanlinju/Anime"
+const val CHECK_UPDATE_ADDRESS = "https://api.github.com/repos/lanlinju/Anime/releases/latest"
 
 const val ANIME_DATABASE = "anime_database.db"
 const val FAVOURITE_TABLE = "favourite_table"
