3a8f594a9833deb3484106997fbd2f465a802f8a
Lanlinju <lanlinju@outlook.com>
2024-09-14 07:59:04 +0800

    chore: release v1.2.2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "com.sakura.anime"
         minSdk = 26
         targetSdk = 35
-        versionCode = 22
-        versionName = "1.2.1"
+        versionCode = 23
+        versionName = "1.2.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
