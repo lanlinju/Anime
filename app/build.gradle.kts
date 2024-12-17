d03c50fe4902539d032163164b67661ade3f916a
Lanlinju <lanlinju@outlook.com>
2024-12-17 17:18:43 +0800

    chore: Release v1.3.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "com.lanlinju.animius"
         minSdk = 26
         targetSdk = 35
-        versionCode = 30
-        versionName = "1.2.9"
+        versionCode = 31
+        versionName = "1.3.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
